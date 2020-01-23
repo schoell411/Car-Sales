d132e6f457c2599a774fff40ea8478880918c340
Kevin Schoell <schoell411@gmail.com>
2020-01-23 15:39:58 -0600

    removed unnecessary code

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,14 +1,8 @@
-const REMOVE_FEATURE = 'REMOVE_FEATURE';
 const removeFeature = item => {
-  return  { type: REMOVE_FEATURE, payload : item }
+  return  { type: 'REMOVE_FEATURE', payload : item }
 }
-
-const ADD_FEATURE = 'ADD_FEATURE';
 const addFeature = item => {
-  return { type: ADD_FEATURE, payload : item }
+  return { type: 'ADD_FEATURE', payload : item }
 }
-
-export { REMOVE_FEATURE };
 export { removeFeature };
-export { ADD_FEATURE };
 export { addFeature };
